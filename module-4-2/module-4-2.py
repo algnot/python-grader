7c86d33895bfe0db8ccbfd1b5d777c48e53ad12f
algnot <thanawat.tongla@gmail.com>
2024-12-21 01:50:38 +0700

    add

--- module-4-2/module-4-2.py
+++ module-4-2/module-4-2.py
@@ -1,8 +1,6 @@
 
 def input_func(input):
     start , end = map(int, input.split(":"))
-    sum = 0
-    for i in range(start, end+1):
-        sum += i
+    n = end - start + 1
 
-    return sum
+    return (n / 2) * (2*start + (n-1))
